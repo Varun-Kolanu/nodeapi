a322e70a4631166087ef8659cfb9c37a71fa9871
Varun-Kolanu <112728411+Varun-Kolanu@users.noreply.github.com>
2023-05-13 01:31:30 +0530

    Try catch added

--- controllers/task.js
+++ controllers/task.js
@@ -1,42 +1,58 @@
 import ErrorHandler from "../middlewares/error.js";
-import {Task} from "../models/task.js";
+import { Task } from "../models/task.js";
 import jwt from "jsonwebtoken";
 import sendTrue from "../utils/sendJson.js";
 
-export const addTask = async (req, res) => {
-    const {title, description} = req.body;
-    const task = await Task.create({title,description, user: req.user});
-    sendTrue(res,"Task created successfully");
+export const addTask = async (req, res, next) => {
+    try {
+        const { title, description } = req.body;
+        const task = await Task.create({ title, description, user: req.user });
+        sendTrue(res, "Task created successfully");
+    } catch (error) {
+        next(error);
+    }
 };
 
-export const showTasks = async (req, res) => {
-    const tasks = await Task.find({user: req.user});
-    res.json({
-        success: true,
-        tasks
-    });
+export const showTasks = async (req, res, next) => {
+    try {
+        const tasks = await Task.find({ user: req.user });
+        res.json({
+            success: true,
+            tasks
+        });
+    } catch (error) {
+        next(error);
+    }
 }
 
 export const updateTask = async (req, res, next) => {
-    const task = await Task.findById(req.params.id);
-    if(!task) return next(new ErrorHandler("Task not found", 404))
-    task.isCompleted = !task.isCompleted;
-    console.log(task);
-    await task.save();
-    res.status(200).json({
-        success: true,
-        message: "Task updated successfully"
-    });
+    try {
+        const task = await Task.findById(req.params.id);
+        if (!task) return next(new ErrorHandler("Task not found", 404))
+        task.isCompleted = !task.isCompleted;
+        console.log(task);
+        await task.save();
+        res.status(200).json({
+            success: true,
+            message: "Task updated successfully"
+        });
+    } catch (error) {
+        next(error);
+    }
 }
 
 export const deleteTask = async (req, res, next) => {
-    const task = await Task.findById(req.params.id);
-    if(!task) return next(new Error(`Task not found`))
-    await task.deleteOne();
-    res.status(200).json({
-        success: true,
-        message: "Task deleted successfully"
-    });
+    try {
+        const task = await Task.findById(req.params.id);
+        if (!task) return next(new Error(`Task not found`))
+        await task.deleteOne();
+        res.status(200).json({
+            success: true,
+            message: "Task deleted successfully"
+        });
+    } catch (error) {
+        next(error);
+    }
 }
 
 
--- controllers/user.js
+++ controllers/user.js
@@ -1,40 +1,57 @@
 import { User } from "../models/user.js";
-import bcrypt from"bcrypt";
+import bcrypt from "bcrypt";
 import { sendCookie } from "../utils/sendCookie.js";
 import jwt from "jsonwebtoken";
 import ErrorHandler from "../middlewares/error.js";
+import sendTrue from "../utils/sendJson.js";
 
 export const register = async (req, res, next) => {
-    const {username,email,password} = req.body;
-    let user = await User.findOne({email});
-    if(user) return next(new ErrorHandler("User already exists", 400));
-    const hashedPwd = await bcrypt.hash(password,10);
-    user = await User.create({username, email, password: hashedPwd});
-    sendCookie(user,res,"You have registered successfully", 201);
+    try {
+        const { username, email, password } = req.body;
+        let user = await User.findOne({ email });
+        if (user) return next(new ErrorHandler("User already exists", 400));
+        const hashedPwd = await bcrypt.hash(password, 10);
+        user = await User.create({ username, email, password: hashedPwd });
+        sendCookie(user, res, "You have registered successfully", 201);
+    } catch (error) {
+        next(error);
+    }
 };
 
 export const login = async (req, res, next) => {
-    const {email, password} = req.body;
-    const user = await User.findOne({email}).select("+password");
-    if(!user) return next(new ErrorHandler("User does not exists", 404));
-    const isMatch = await bcrypt.compare(password, user.password);
-    if(!isMatch) return next(new ErrorHandler("Invalid email or password"));
-    sendCookie(user,res,`Welcome back ${user.username}`,200);
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email }).select("+password");
+        if (!user) return next(new ErrorHandler("User does not exists", 404));
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return next(new ErrorHandler("Invalid email or password"));
+        sendCookie(user, res, `Welcome back ${user.username}`, 200);
+    } catch (error) {
+        next(error);
+    }
 }
 
-export const logout = async (req, res) => {
-    res.cookie("token",null, {
-        expires: new Date(Date.now())
-    })
-    sendTrue(res,"Logged out successfully");
+export const logout = async (req, res, next) => {
+    try {
+        res.cookie("token", null, {
+            expires: new Date(Date.now())
+        })
+        sendTrue(res, "Logged out successfully");
+    } catch (error) {
+        next(error);
+    }
 }
 
 export const myInfo = async (req, res) => {
-    res.status(200)
-    .json({
-        success: true,
-        user: req.user
-    })
+    try {
+        res.status(200)
+            .json({
+                success: true,
+                user: req.user
+            })
+    } catch (error) {
+        next(error);
+    }
 }
 
 // export const allUsers = async (req, res) => {
